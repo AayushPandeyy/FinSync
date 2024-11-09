8813fe0785565d943f21e8efa10e5bfb03c8389c
AayushPandeyy <ap200061@gmail.com>
2024-11-09 11:26:35 +0545

    Firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:finance_tracker/firebase_options.dart';
 import 'package:finance_tracker/pages/NavigatorPage.dart';
 import 'package:finance_tracker/pages/auth/LoginPage.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
